57b5956cb80dc776b422707d37ab79fc812d57c6
Ryan Lee <ryantlee9@gmail.com>
2016-10-21 00:58:08 -0400

    Use curl and fastjar instead of wget and unzip

--- get_word_vecs_init.sh
+++ get_word_vecs_init.sh
@@ -1,6 +1,6 @@
 !# /bin/bash
 
-wget http://nlp.stanford.edu/data/glove.6B.zip
-unzip glove.6B.zip
+curl -s http://nlp.stanford.edu/data/glove.6B.zip | bash
+fastjar xvf glove.6B.zip
 mkdir util/glove
 cp glove.6B.200d.txt util/glove/vectors.6B.200d.txt
