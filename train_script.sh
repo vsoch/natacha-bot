7b0a6341750a7e8749eb8e259c342bf4b1506a1d
Ryan Lee <ryantlee9@gmail.com>
2016-10-22 08:56:16 -0400

    Update training parameters
    Move to word based model
    Increase training portion
    Introduce dropouts
    Decrease epochs and printing

--- train_script.sh
+++ train_script.sh
@@ -1,3 +1,16 @@
 !# /bin/bash
 
-th train.lua -gpuid -1 -data_dir data/trump  #-rnn_size 512 -num_layers 2 -dropout 0.5
+th train.lua \
+	-gpuid -1 \
+	-data_dir data/trump  \
+	-train_frac 0.96 \
+	-val_frac 0.04 \
+	-max_epochs 30 \
+	-word_level 1 \
+	-threshold 2 \
+	-print_every 10 \
+	-glove 1 \
+	-rnn_size 128 \
+	-num_layers 2 \
+	-dropout 0.5 \
+	# -recurrent_dropout 0.5 \
