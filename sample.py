058efc1bb8168b9a06c390cba7ef814a4f36f451
Ryan Lee <ryantlee9@gmail.com>
2016-10-27 00:55:00 -0400

    Use char-level model to complete first sentence
    Hand over to word-level model after char-level model completes first
    sentence

--- sample.py
+++ sample.py
@@ -4,32 +4,80 @@ import random
 import string
 
 
-def get_sample(seed_text=None):
+class Sampler():
 
-    # Generate random character if none provided
-    if seed_text is None:
-        seed_text = random.choice(string.letters).upper()
+    def __init__(self, seed_text=None,
+                 char_temperature=.4, word_temperature=.75):
+        # Generate random character for the seed if none provided
+        if seed_text is None:
+            seed_text = random.choice(string.letters).upper()
+        self.seed = seed_text
+        self.char_temperature = char_temperature
+        self.word_temperature = word_temperature
 
-    # Sample from the model using provided seed
-    sample = subprocess.check_output([
-        'th', 'sample.lua',
-        'cv/word-rrn-trained.t7',
-        '-temperature', '.75',
-        '-length', '500',
-        '-gpuid', '-1',
-        '-primetext', seed_text
-    ])
+    def get_sample_raw(self, model_type, seed, temperature, length):
+        # Generate random character if none provided
+        if seed is None:
+            seed = random.choice(string.letters).upper()
 
-    # Return cleaned sampled text
-    sample_clean = str(sample).split("--------------------------")[1]
-    sample_clean = sample_clean.replace(" ' ", "'")
-    sample_clean = sample_clean.replace(" .", ".").replace(" ?", "?").\
-        replace(" !", "!").replace(" ,", ",").replace(" ;", ";")
-    sample_clean = ' '.join(sample_clean.split())
-    return seed_text.strip() + ' ' + sample_clean
+        # Get model name
+        if model_type == 'word':
+            model_name = 'cv/word-rnn-trained.t7'
+        elif model_type == 'char':
+            model_name = 'cv/char-rnn-trained.t7'
+        else:
+            raise ValueError('Model type {} not supported'.format(model_type))
+
+        # Sample from the model using provided seed
+        sample = subprocess.check_output([
+            'th', 'sample.lua',
+            model_name,
+            '-temperature', str(temperature),
+            '-length', str(length),
+            '-gpuid', '-1',
+            '-primetext', seed
+        ])
+
+        # Return cleaned sampled text
+        return self.denormalize(sample)
+
+    # Denormalize sampled text
+    def denormalize(self, sample):
+        sample_clean = str(sample).split("--------------------------")[1]
+        sample_clean = sample_clean.replace(" . . . ", "... ")
+        sample_clean = sample_clean.\
+            replace(" . ", ". ").\
+            replace(" \ ? ", "? ").\
+            replace(" ! ", "! ").\
+            replace(" ' ", "'").\
+            replace(" , ", ", ").\
+            replace("- -", "--").\
+            replace(" ; ", "; ")
+        sample_clean = sample_clean.replace(" n't", "n't")
+        return sample_clean
+
+    def get_sample(self):
+        if self.seed.strip()[-1:] != '.':
+            char_sample = self.get_sample_raw(
+                'char', self.seed, self.char_temperature, 300)
+            char_sample_clean = ' '.join([
+                word.replace(' ', '')
+                for word in char_sample.split('   ')])
+            if char_sample[0] != ' ':
+                char_sample_clean = char_sample_clean.strip()
+            sample_starter = self.seed + \
+                char_sample_clean.split('.')[0] + '.'
+        else:
+            sample_starter = self.seed
+
+        word_sample = self.get_sample_raw(
+            'word', sample_starter, self.word_temperature, 500)
+        sample_clean = sample_starter + ' ' + word_sample
+        return ' '.join(sample_clean.split())
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        print get_sample(sys.argv[1])
+        sampler = Sampler(sys.argv[1])
     else:
-        print get_sample()
+        sampler = Sampler()
+    print sampler.get_sample()
