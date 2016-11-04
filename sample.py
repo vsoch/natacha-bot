0e906865e67621d9bd7f91642508bfa8e6ab426d
Ryan Lee <ryantlee9@gmail.com>
2016-11-04 01:06:39 -0400

    Update sampling script
    Fix white space control for char-level generation

--- sample.py
+++ sample.py
@@ -11,7 +11,11 @@ class Sampler():
         # Generate random character for the seed if none provided
         if seed_text is None:
             seed_text = random.choice(string.letters).upper()
-        self.seed = seed_text
+        if seed_text[-1:] == ' ':
+            seed_text_trimmed = seed_text.strip() + ' '
+        else:
+            seed_text_trimmed = seed_text.strip()
+        self.seed = seed_text_trimmed
         self.char_temperature = char_temperature
         self.word_temperature = word_temperature
 
@@ -56,23 +60,45 @@ class Sampler():
         sample_clean = sample_clean.replace(" n't", "n't")
         return sample_clean
 
+    # Sample text from model
     def get_sample(self):
+
         if self.seed.strip()[-1:] != '.':
+            # If seed is not a full sentence
+
+            # Sample from character model
             char_sample = self.get_sample_raw(
                 'char', self.seed, self.char_temperature, 300)
+
+            # Remove start and end newlines
+            char_sample = char_sample.split('\n')[1]
+
+            # Remove extra spacing
             char_sample_clean = ' '.join([
                 word.replace(' ', '')
-                for word in char_sample.split('   ')])
-            if char_sample[0] != ' ':
+                for word in char_sample.split('   ')]).strip()
+
+            # Preserve initial spacing if relevant
+            if char_sample[0] != char_sample_clean[0]:
+                char_sample_clean = ' ' + char_sample_clean.strip()
+            else:
                 char_sample_clean = char_sample_clean.strip()
+
+            # Take only the first sentence from the char-level sample
             sample_starter = self.seed + \
                 char_sample_clean.split('.')[0] + '.'
+
         else:
+            # Seed is a full sentence, so skip char-level model
             sample_starter = self.seed
 
+        # Sample from the word-level model
         word_sample = self.get_sample_raw(
             'word', sample_starter, self.word_temperature, 500)
+
+        # Join char-level sample with word-level sample
         sample_clean = sample_starter + ' ' + word_sample
+
         return ' '.join(sample_clean.split())
 
 if __name__ == '__main__':
